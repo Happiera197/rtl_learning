1a77a730e135c43571a1a075a26d7b024561b2c1
Happiera197 <17719945156@qq.com>
2026-07-22 00:29:35 +0800

    week1_finish

--- week1_summary_ALU/rtl/ALU.sv
+++ week1_summary_ALU/rtl/ALU.sv
@@ -24,15 +24,16 @@ logic [WIDTH:0] EX_result;
 always_comb begin
     carry           = 1'b0;
     overflow        = 1'b0;
+    EX_result = '0;
     case(op)
     OP_ADD:begin
-        EX_result={1,b0,a}+{1'b0,b};
+        EX_result={1'b0,a}+{1'b0,b};
         result=EX_result[WIDTH-1:0];
         carry=EX_result[WIDTH];
         overflow=(~(a[WIDTH-1]^b[WIDTH-1]))&(result[WIDTH-1]^a[WIDTH-1]);
     end
     OP_SUB:begin
- EX_result={1,b0,a}+{1'b0,~b}+1'b1;
+ EX_result={1'b0,a}+{1'b0,~b}+1'b1;
         result=EX_result[WIDTH-1:0];
         carry=EX_result[WIDTH];
         overflow=(a[WIDTH-1]^b[WIDTH-1])&(result[WIDTH-1]^a[WIDTH-1]);
@@ -40,12 +41,12 @@ always_comb begin
     OP_AND:result=a&b;
     OP_OR:result=a|b;
     OP_XOR:result=a^b;
-    OP_SLT:result={(WIDTH-1){1'b0},$signed(a)<$signed(b)};
-    OP_SLTU:result={(WIDTH-1){1'b0},a<b};
+    OP_SLT:result={{(WIDTH-1){1'b0}},($signed(a)<$signed(b))};
+    OP_SLTU:result={{(WIDTH-1){1'b0}},(a<b)};
     OP_SHL:result = a << 1;
     OP_SHR:result = a >> 1;
-    default:result=WIDTH{1'b0};
+    default:result={WIDTH{1'b0}};
     endcase
 end
-assgin zero=(result=='0);
+assign zero=(result=='0);
 endmodule
